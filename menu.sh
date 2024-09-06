7c244caff160848bee7627d0da568ac820045fb6
Tjelite <113060877+tjelite1986@users.noreply.github.com>
2024-09-06 19:11:23 +0200

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,65 +1,48 @@
 #!/bin/bash
 
-# creating a menu with the following options
-echo "Choose were to create symlinks";
-echo "1. Create Symlinks Blackhole Radar"
-echo "2. Create Symlinks Blackhole Sonarr"
-echo "3. Create Symlinks Blackhole All"
-echo "4. Create Symlinks Import Movies"
-echo "5. Create Symlinks Import Shows"
-echo "6. Create Symlinks Import All"
-echo "7. Check for broken Symlinks"
-echo "8. Remove broken Symlinks"
-echo "9. Exit from menu "
-echo -n "Enter your menu choice [1-9]: "
+# Function to display menu
+display_menu() {
+
+  echo -e "\e[1;34mChoose where to create symlinks:\e[0m"  # Bold Blue
+  echo -e "\e[1;32m1. Create Symlinks Blackhole Radar\e[0m"  # Bold Green
+  echo -e "\e[1;32m2. Create Symlinks Blackhole Sonarr\e[0m"
+  echo -e "\e[1;32m3. Create Symlinks Blackhole All\e[0m"
+  echo -e "\e[1;32m4. Create Symlinks Import Movies\e[0m"
+  echo -e "\e[1;32m5. Create Symlinks Import Shows\e[0m"
+  echo -e "\e[1;32m6. Create Symlinks Import All\e[0m"
+  echo -e "\e[1;31m7. Check for broken Symlinks\e[0m"  # Bold Red
+  echo -e "\e[1;31m8. Remove broken Symlinks\e[0m"
+  echo -e "\e[1;33m9. Exit from menu\e[0m"  # Bold Yellow
+  echo -n "Enter your menu choice [1-9]: "
+}
 
 # Running a forever loop using while statement
-# This loop will run until select the exit option.
-# User will be asked to select option again and again
-while :
-do
+while :; do
+  display_menu  # Call the display menu function
+  
+  # Reading choice
+  read choice
 
-# reading choice
-read choice
-
-# case statement is used to compare one value with the multiple cases.
-case $choice in
-  # Pattern 1
-  1)  ./blackhole-radarr.sh
-     echo "You Symlinks are done ";;
-  # Pattern 2
-  2)  ./blackhole-sonarr.sh
-      echo "You Symlinks are done ";;
-  3)  ./blackhole.sh
-     echo "You Symlinks are done ";;
-  # Pattern 3
-  4)  ./import-movies.sh
-      echo "You Symlinks are done ";;
-    # Pattern 4
-  5)  ./import-shows.sh
-      echo "You Symlinks are done ";;
-  6)  ./import.sh
-      echo "You Symlinks are done ";;
-  7)  ./broken.sh
-      echo "Check For Broken Symlinks Done ";;
-  8)  ./remove.sh
-      echo "Remove Broken Symlinks Done ";;
-  # Pattern 5
-  9)  echo "Quitting ..."
-      exit;;
-  # Default Pattern
-  *) echo "invalid option";;
-
-esac
-echo "Choose were to create symlinks";
-echo "1. Create Symlinks Blackhole Radar"
-echo "2. Create Symlinks Blackhole Sonarr"
-echo "3. Create Symlinks Blackhole All"
-echo "4. Create Symlinks Import Movies"
-echo "5. Create Symlinks Import Shows"
-echo "6. Create Symlinks Import All"
-echo "7. Check for broken Symlinks"
-echo "8. Remove broken Symlinks"
-echo "9. Exit from menu "
-echo -n "Enter your menu choice [1-9]: "
+  # Case statement to execute based on user choice
+  case $choice in
+    1)  ./blackhole-radarr.sh
+        echo -e "\e[1;32mSymlinks for Blackhole Radar are done.\e[0m";;
+    2)  ./blackhole-sonarr.sh
+        echo -e "\e[1;32mSymlinks for Blackhole Sonarr are done.\e[0m";;
+    3)  ./blackhole.sh
+        echo -e "\e[1;32mSymlinks for Blackhole All are done.\e[0m";;
+    4)  ./import-movies.sh
+        echo -e "\e[1;32mSymlinks for importing movies are done.\e[0m";;
+    5)  ./import-shows.sh
+        echo -e "\e[1;32mSymlinks for importing shows are done.\e[0m";;
+    6)  ./import.sh
+        echo -e "\e[1;32mSymlinks for importing all are done.\e[0m";;
+    7)  ./broken.sh
+        echo -e "\e[1;31mCheck for broken symlinks done.\e[0m";;
+    8)  ./remove.sh
+        echo -e "\e[1;31mRemove broken symlinks done.\e[0m";;
+    9)  echo -e "\e[1;33mQuitting...\e[0m"
+        exit;;
+    *)  echo -e "\e[1;31minvalid option. Please try again.\e[0m";;
+  esac
 done
